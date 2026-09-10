77bdeb7316aa0ecc3d0d660a722d2a7820f139c6
devbidden <obunadikechioma8@gmail.com>
2026-09-10 10:55:25 +0100

    Updated

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const connectcloudinary = require("./config/cloudinary").connectcloudinary;
 const { verifyEmailTransport } = require("./utils/email");
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
-const webhook = require("./rapi/webhook");
 
 async function startServer() { 
     try {
