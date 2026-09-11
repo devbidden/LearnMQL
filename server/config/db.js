9ebcd45e94f71f1642a2f9df4b822cc0347e2cef
devbidden <obunadikechioma8@gmail.com>
2026-09-11 15:46:04 +0100

    new updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,25 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    console.log("--- MongoDB Debug ---");
+    console.log("MONGODB_URL exists:", !!process.env.MONGODB_URL);
+    console.log("MongoDB readyState before:", mongoose.connection.readyState);
+
     if (!process.env.MONGODB_URL) {
         throw new Error("MONGODB_URL is not configured");
     }
 
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log("MongoDB connected");
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        console.log("MongoDB connected successfully");
+        console.log("MongoDB readyState after:", mongoose.connection.readyState);
+    } catch (error) {
+        console.error("MongoDB connection failed:");
+        console.error(error);
+
+        throw error;
+    }
 };
 
 module.exports = connectDB;
